9ca8912b735814f7430a984ee9713bc9b2018f90
Ar355 <zeneli.arian@gmail.com>
2020-08-21 20:15:59 +0000

    create tb user, address, cars

---
+++ db/schema/users_address_cars.sql
@@ -0,0 +1,31 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  address_id INTEGER REFERENCES addresses(id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE addresses (
+  id SERIAL PRIMARY KEY NOT NULL,
+  country VARCHAR(255) NOT NULL,
+  province VARCHAR(255) NOT NULL,
+  city VARCHAR(255) NOT NULL,
+  street VARCHAR(255) NOT NULL,
+  postal_codode VARCHAR(255) NOT NULL,
+);
+
+CREATE TABLE cars (
+  make VARCHAR(255) NOT NULL,
+  model VARCHAR(255) NOT NULL,
+  year SMALLINT NOT NULL,
+  mileage SMALLINT NOT NULL,
+  description TEXT,
+  price INTEGER,
+  address_id INTEGER REFERENCES addresses(id) ON DELETE CASCADE,
+  image_url VARCHAR(255) NOT NULL,
+  availability BOOLEAN,
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+);
+
