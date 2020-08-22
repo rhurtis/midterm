df24e7b76cd85848ec972c9cb2ccf386f046447a
Ryan H <rhurtis@hotmail.com>
2020-08-22 03:55:49 +0000

    Added 10 cars to the cars table.

---
+++ db/schema/03_cars.sql
@@ -0,0 +1,14 @@
+/* Inserting data into the cars table*/
+INSERT INTO cars (make, model, year, mileage, price, address_id, image_url, owner_id)
+VALUES ('Mitsubishi', 'Lancer Evolution IX', 2006, 80000, 20000, 1,'url coming',1),
+('Nissan', 'GT-R', 2010, 40000, 60000, 2,'url coming',2),
+('Honda', 'Civic', 2018, 30000, 15000, 3,'url coming',3),
+('Toyota', 'Corolla', 2005, 195000, 5500, 4,'url coming',4),
+('Lamborghini', 'Aventador', 2015, 5000, 200000, 5,'url coming',5),
+('Toyota', 'Supra', 1994, 10000, 80000, 6,'url coming',6),
+('Ford', 'F-150', 2011, 75000, 14000, 7,'url coming',7),
+('Honda', 'Odyssey', 1995, 300000, 2000, 8,'url coming',8),
+('Pontiac', 'Aztec', 2003, 225000, 3500, 9,'url coming',9),
+('DMC', 'DeLorean', 1982, 1337, 500000, 10,'url coming',10);
+
+
