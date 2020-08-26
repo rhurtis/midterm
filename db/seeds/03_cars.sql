c9059eada181ac4f39e9f99b3254ea920d56b3a7
Angel <angel.ho@ryerson.ca>
2020-08-26 23:27:08 +0000

    added description on cars database, uncommented for users database, up to date master schema

--- db/seeds/03_cars.sql
+++ db/seeds/03_cars.sql
@@ -1,14 +1,26 @@
 /* Inserting data into the cars table*/
-INSERT INTO cars (id, make, model, year, mileage, price, image_url, owner_id)
-VALUES (1,'Mitsubishi', 'Lancer Evolution IX', 2006, 80000, 20000,'url coming',1),
-(2, 'Nissan', 'GT-R', 2010, 40000, 60000,'url coming',2);
--- (3, 'Honda', 'Civic', 2018, 30000, 15000, 3,'url coming',3),
--- (4, 'Toyota', 'Corolla', 2005, 195000, 5500, 4,'url coming',4),
--- (5, 'Lamborghini', 'Aventador', 2015, 5000, 200000, 5,'url coming',5),
--- (6, 'Toyota', 'Supra', 1994, 10000, 80000, 6,'url coming',6),
--- (7, 'Ford', 'F-150', 2011, 75000, 14000, 7,'url coming',7),
--- (8, 'Honda', 'Odyssey', 1995, 300000, 2000, 8,'url coming',8),
--- (9, 'Pontiac', 'Aztec', 2003, 225000, 3500, 9,'url coming',9),
--- (10, 'DMC', 'DeLorean', 1982, 1337, 500000, 10,'url coming',10);
+INSERT INTO cars (id, make, model, year, mileage, description, price, image_url, owner_id)
+VALUES (1,'Mitsubishi', 'Lancer Evolution IX', 2006, 80000, 'Carfax checked! For the 2006 model year, all Lancers get an updated front fascia and interior enhancements. A black mesh grille is now installed on O-Z Rally and Ralliart models, while the ES has a chrome grille. Daytime running lights are now standard. A Security Package for ES and O-Z Rally models includes antilock brakes and side-impact airbags for the front seats.', 20000,'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS5nzIkWOSrZDiK2awIfBeUcjCC0VOFiZcqCQ&usqp=CAU',1),
+
+(2, 'Nissan', 'GT-R', 2010, 40000, 'Carfax checked! 3.8L V6 Twin-turbo (485 hp and 434 ft-lbs. torque) All Wheel Drive Sequential 6-speed manual dual-clutch paddle-shift, driver configurable transmission w/automatic mode Bose audio system with Music Box hard-drive storage Super Silver Metallic paint ($3,000 option)
+Black leather interior', 60000,'https://mywishcard.com/s/i2/c0/4/470x0_5b4N6VpxcV4AhqkTbF8uxNKhu1E4UN2N___jpg____4_a96bfb5a.jpg',2),
+
+(3, 'Honda', 'Civic', 2018, 30000, 'Carfax checked! 2nd Owner, NO ACCIDENTS, LOW KM, well maintained all maintenance records will be provided, by Honda. w/ Navigation, Back-Up Camera, Carbon Fiber Trim, Lane Assist Camera, Proximity Entry, Push To Start, Red/Black 2 Tone Seats, Power Locks, Power Windows, Sunroof, Bluetooth Comes with two sets of tires, winter and summer Engine: 2.4L I-4 DOHC 16-Valve i-VTEC
+L/100Km City: 9.9 L/100Km Hwy: 6.6 ', 15000, 'http://i.imgur.com/CtTiqoC.jpg',3),
+
+(4, 'Toyota', 'Corolla', 2005, 195000, 'Mint condition, well maintained, new snow tires, low mileage 155,000, ice cold air, fully certified', 5500,'https://tdrpmimages.azureedge.net/private/2020/7/10/60f2d73d-46e8-4c7b-ae34-c473a6c8c0c4.jpg-1024x786',4),
+
+(5, 'Lamborghini', 'Aventador', 2015, 5000, 'Equipped with Transparent Engine Bonnet, Red Brake Calipers, Magneto-Rheological Suspension with Red Rear Springs with Lifting System and many other features! Visit Grand Touring Automobiles at 777 Dundas Street, Toronto to explore this SV in-person. Some of the other options on this Lamborghini include: - Sensonum Premium Sound System- Rear View Camera- Homelink- Fully Electric and Heated Seats with Detail in Carbon Fiber- Multifunction Steering Wheel in Alcantara with Perforated Leather Inserts- Branding Package with Alcantara- Travel Package- Fire Extinguisher- Footplate in Carbon Fiber This SV is stunning in every way. The Bianco Isis exterior color is extremely vibrant, and will stand out in all situations. Under the hood is the powerful 6.5L V12 engine that produces 750 horsepower. A 7-speed transmission delivers this power to an all wheel drive system and allows you to accelerate from 0-100 km/h in a blistering 2.9 seconds.', 200000,'https://tdrpmimages.azureedge.net/photos/import/202008/0513/2210/8992470d-ed70-48c1-89f0-aec94b0c8eb5.jpg-1024x786',5),
+
+(6, 'Toyota', 'Supra', 1994, 10000, 'Up for sale is my 1994 Toyota Supra RZ 6 Speed (original twin turbo) Imported by B Pro, Auction Grade 4 with no accidents and a clean title. I have all documentation available (auction, import, and service records) Fed legal, ready for import to USA with 25 year rule (will help buyer with transportation to border) Alpine silver metallic 199  141 000 original KMs
+BPU (Basic power upgrade): ETS 5"intercooler with 3" aluminum charge pipe', 80000, 'https://tdrpmimages.azureedge.net/private/2020/8/24/f6cf1633-27f1-45cd-b078-ee83b48dc577.jpg-1024x786',6),
+
+(7, 'Ford', 'F-150', 2011, 75000, 'Buy it or leave it!', 14000, 'https://tdrpmimages.azureedge.net/private/2020/8/20/83ec68b6-ea49-4c68-bbaa-272a979c0d10.jpeg-1024x786',7),
+
+(8, 'Honda', 'Odyssey', 1995, 300000,' This is Henry. 1) he needs a new front left lug wheel nut 2) no AC (can be fixed with a new AC belt as advised by the mechanic. I did not fix it due to flying out of the country) 3) crack across the window screen (has not moved in the 3yrs ive owned him)', 2000, 'https://tdrpmimages.azureedge.net/private/2020/8/7/0feecf7a-f30f-4281-9144-850937e6d3f3.jpg-1024x786',8),
+
+(9, 'Pontiac', 'Aztec', 2003, 225000,'Engine	3.4L V-6 MPG	19 City / 26 Hwy Seating	5 Passengers Transmission	4-spd auto w/OD Power	185 @ 5200 rpm Drivetrain	front-wheel', 3500, 'https://s.aolcdn.com/dims-global/dims3/GLOB/legacy_thumbnail/788x525/quality/85/https://s.aolcdn.com/commerce/autodata/images/CAB30POS011A0101.jpg',9),
+
+(10, 'DMC', 'DeLorean', 1982, 1337, 'The DMC DeLorean (often referred to simply as the "DeLorean") is a sports car and the only automobile manufactured by John DeLoreans DeLorean Motor Company (DMC) for the American market from 1981 to 1983. The car was designed by Giorgetto Giugiaro and stood out for its gull-wing doors and brushed stainless-steel outer body panels. It became widely known for its disappointing lack of power and performance, which did not match the expectations created by its looks and price tag, but it is memorable for its appearances as the time machine in the Back to the Future media franchise after 1985.', 500000, 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/De_Lorean_DMC-12_at_Wollaton_Park%2C_Nottingham%2C_England.jpg/220px-De_Lorean_DMC-12_at_Wollaton_Park%2C_Nottingham%2C_England.jpg',10);
 
 
